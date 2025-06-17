fc060eb81659249cb00e4ebde56c7496ea3d496b
Krina Suthar <krina.suthar@vrindimail.com>
2025-06-17 11:35:29 +0530

    Changes to controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace TestApp.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            //testing
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
